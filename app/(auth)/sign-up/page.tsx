0623bc918bfbeb01792db83e967efef8f9e06d07
rajarya199 <aryalrajan078@gmail.com>
2025-05-17 14:26:49 +0545

    sign-up

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthForm from '@/components/auth/AuthForm'
+
+const page = () => {
+  return (
+    <>
+    <AuthForm type='register'/></>
+  )
+}
+
+export default page

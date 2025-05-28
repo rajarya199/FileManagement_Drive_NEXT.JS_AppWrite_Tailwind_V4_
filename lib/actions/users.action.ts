46fc2aa75bf4eb6ed9e194b4e0a7a01e3e18a62b
rajarya199 <aryalrajan078@gmail.com>
2025-05-28 18:23:25 +0545

    verify otp

--- lib/actions/users.action.ts
+++ lib/actions/users.action.ts
@@ -30,7 +30,7 @@ const getUserByEmail=async(email:string)=>{
 
 }
 
-const sendEmailOTP=async({email}:{email:string})=>{
+ export const sendEmailOTP=async({email}:{email:string})=>{
   const{account}=await createAdminClient()
   try{
     const session=await account.createEmailToken(ID.unique(),email);
@@ -66,3 +66,29 @@ if(!existingUser){
     return parseStringify({accountId})
 
 }
+
+
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, "Failed to verify OTP");
+  }
+};

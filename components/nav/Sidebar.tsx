42069e42b5ada995f97ee1ca982856d9677b5b03
rajarya199 <aryalrajan078@gmail.com>
2025-05-29 14:42:17 +0545

    sidebar

---
+++ components/nav/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar remove-scrollbar'>
+          <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className='sidebar-nav'>
+                <ul className="flex flex-1 flex-col gap-6">
+</ul>
+
+      </nav>
+      
+      <Image
+        src="/assets/images/files-2.png"
+        alt="logo"
+        width={506}
+        height={418}
+        className="w-full"
+      />
+      </aside>
+  )
+}
+
+export default Sidebar

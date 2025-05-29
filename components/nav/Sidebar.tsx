564aeea2654cbc58c16a4977828971717eebebde
rajarya199 <aryalrajan078@gmail.com>
2025-05-29 22:53:39 +0545

    sidebar

--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -1,8 +1,14 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { navItems } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
   return (
     <aside className='sidebar remove-scrollbar'>
           <Link href="/">
@@ -24,6 +30,28 @@ const Sidebar = () => {
       </Link>
       <nav className='sidebar-nav'>
                 <ul className="flex flex-1 flex-col gap-6">
+                        {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active",
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active",
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
 </ul>
 
       </nav>

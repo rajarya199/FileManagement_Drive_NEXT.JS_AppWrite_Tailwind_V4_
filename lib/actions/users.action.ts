19c04d28b3c5775fc976274987df29b7254a0771
rajarya199 <aryalrajan078@gmail.com>
2025-05-30 23:15:35 +0545

    get user

--- lib/actions/users.action.ts
+++ lib/actions/users.action.ts
@@ -92,3 +92,28 @@ export const verifySecret = async ({
     handleError(error, "Failed to verify OTP");
   }
 };
+
+export const getCurrentUser=async()=>{
+   try{
+      const{databases,account}=await createSessionClient();
+  const result=await account.get();
+  
+    const user = await databases.listDocuments(
+     
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal("accountId", result.$id)],
+    );
+     if (user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+   }
+      catch(error){
+            console.log(error);
+
+      }
+
+
+   
+
+}

0a0800838d321d531f10e295fef9a623ed8c31a7
rajarya199 <aryalrajan078@gmail.com>
2025-05-31 15:58:27 +0545

    cureent user info

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from '@/components/nav/Sidebar'
 import Header from '@/components/nav/Header'
+import { getCurrentUser } from '@/lib/actions/users.action'
+import { redirect } from 'next/navigation';
 const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser=await getCurrentUser()
+  if(!currentUser){
+    return redirect('/sign-in')
+  }
   return (
     <main className='flex h-screen'>
-        <Sidebar/>
+        <Sidebar {...currentUser}/>
         <section className="flex h-full flex-1 flex-col" >
         <Header/>
                 <div className="main-content">{children}</div>

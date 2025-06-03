a7bdfedf561d3c840878ac03ddd6629af254d5a3
rajarya199 <aryalrajan078@gmail.com>
2025-06-03 21:34:16 +0545

    signin

--- lib/actions/users.action.ts
+++ lib/actions/users.action.ts
@@ -131,3 +131,20 @@ export const signOutuser=async()=>{
     redirect("/sign-in");
   }
 }
+
+export const signInUser=async({email}:{email:string})=>{
+try{
+     const existingUser = await getUserByEmail(email);
+
+    // User exists, send OTP
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+        return parseStringify({ accountId: null, error: "User not found" });
+
+}
+catch(error){
+ handleError(error, "Failed to sign in user");
+}
+}

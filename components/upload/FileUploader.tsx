2acbfa2f3585bbe230ecaf8a9fea524bc1f67ca8
rajarya199 <aryalrajan078@gmail.com>
2025-06-04 22:30:41 +0545

    upload files

--- components/upload/FileUploader.tsx
+++ components/upload/FileUploader.tsx
@@ -6,6 +6,10 @@ import { Button } from '../ui/button';
 import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
 import Image from 'next/image';
 import Thumbnail from '../Thumbnail';
+import { MAX_FILE_SIZE } from '@/constants';
+import { toast } from "sonner";
+import { uploadFile } from '@/lib/actions/files.action';
+
 interface Props {
   ownerId: string;
   accountId: string;
@@ -18,6 +22,32 @@ const FileUploader = ({ownerId,accountId,className}:Props) => {
  const onDrop=useCallback(
     async(acceptedFiles:File[])=>{
         setFiles(acceptedFiles)
+
+        const uploadPromises=acceptedFiles.map(async(file)=>{
+          if(file.size>MAX_FILE_SIZE){
+             setFiles((prevFiles) =>
+            prevFiles.filter((f) => f.name !== file.name),
+          );
+           return  toast.error(
+          <p className="body-2 text-white">
+            <span className="font-semibold">{file.name}</span> is too large.
+            Max file size is 50MB.
+          </p>
+        )}
+
+          return uploadFile({ file, ownerId, accountId, path }).then(
+          (uploadedFile) => {
+            if (uploadedFile) {
+              setFiles((prevFiles) =>
+                prevFiles.filter((f) => f.name !== file.name),
+              );
+            }
+          },
+        );
+
+        })
+            await Promise.all(uploadPromises);
+
     },[ownerId, accountId, path]
  )
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
@@ -78,12 +108,6 @@ const FileUploader = ({ownerId,accountId,className}:Props) => {
           })}
           </ul>
             )}
-
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
     </div>
   )
 }

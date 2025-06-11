09f4a5a281f80e3ceeee0854ccfa20af034a20c2
rajarya199 <aryalrajan078@gmail.com>
2025-06-11 14:08:45 +0545

    query,get files

--- lib/actions/files.action.ts
+++ lib/actions/files.action.ts
@@ -60,6 +60,9 @@ handleError(error,"Failed to upload file");
 }
 
 const createQueries=(currentUser: Models.Document,  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number,
 )=>{
     const queries = [
     Query.or([
@@ -68,17 +71,26 @@ const createQueries=(currentUser: Models.Document,  types: string[],
     ]),
   ];
   if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
 
+    if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
+  }
   return queries;
 }
 
 //get the files
-export const getFiles=async({  types = []}:GetFilesProps)=>{
+export const getFiles=async({  types = [],searchText='',  sort = "$createdAt-desc",limit}:GetFilesProps)=>{
   const {databases}=await createAdminClient()
   try{
     const currentUser=await getCurrentUser();
     if(!currentUser) throw new Error("User not found");
-    const queries=createQueries(currentUser,types)
+    const queries=createQueries(currentUser,types,searchText,sort,limit)
     const files=await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
